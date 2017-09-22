9cd81bb0d44ddf7770142253c62f1054e1a9761e
Pavel Koníř <pavel@konir-osx.local>
2017-09-22 12:06:15 +0200

    mlat client startup custom script

---
+++ czadsb.custom.mlat-client.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+STATION_LAT=00.00000
+STATION_LON=00.00000
+STATION_ALT=000
+STATION_NAME=“STATION NAME”
+
+while do
+  sleep 5
+  /home/pi/mlat-client/mlat-client --input-type dump1090 --input-connect localhost:30005 --results basestation,connect,czadsb.cz:31003 --lat $STATION_LAT --lon $STATION_LON --alt $STATION_ALT --server czadsb.cz:40147 --user "$STATION_NAME"
+done
+
