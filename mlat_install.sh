576d54e13115f267d0501efdf3609d35107f813c
Pavel Koníř <pavel@konir-osx.local>
2017-10-05 15:47:17 +0200

    module install

---
+++ mlat_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#Install mlat-client
+sudo apt-get install git build-essential debhelper python3-dev -y
+
+cd ~
+git clone https://github.com/mutability/mlat-client.git
+cd mlat-client
+git checkout v0.2.5
+
+dpkg-buildpackage -b -uc
+
+
+sudo dpkg -i ../mlat-client_*.deb
---
+++ piaware_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Add piaware source
+wget http://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_3.5.1_all.$
+sudo dpkg -i piaware-repository_3.5.1_all.deb
+
+#Install piaware
+sudo apt-get update
+sudo apt-get install piaware piaware-config piaware-status dump1090-fa  -y
