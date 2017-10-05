fbdff86dc09a159cc94d5cc3605f018c2024f80d
Pavel Koníř <pavel@konir-osx.local>
2017-10-05 16:06:41 +0200

    piaware packages

---
+++ czadsb-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git
+
+cd ~
+git clone https://github.com/pavelkonir/czadsb-scripts.git
+
+sudo chmod +x ~/czadsb-scripts/*.sh
+
+mkdir ~/downloads
+cd ~/downloads
+
+~/czadsb-scripts/piaware_install.sh
+
+~/czadsb-scripts/mlat_install.sh
+
+#detect hw version and download modesmixer
+
+#hw_revision = cat /proc/cpuinfo | grep Revision| cut -d':' -f 2
+
+
+
+
+
+
