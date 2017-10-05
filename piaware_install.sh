de5463531651c67d12391ff392be86157027785d
Pavel Koníř <pavel@konir-osx.local>
2017-10-05 16:01:45 +0200

    Fix url

--- piaware_install.sh
+++ piaware_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Add piaware source
-wget http://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_3.5.1_all.$
+wget http://flightaware.com/adsb/piaware/files/packages/pool/piaware/p/piaware-support/piaware-repository_3.5.1_all.deb
 sudo dpkg -i piaware-repository_3.5.1_all.deb
 
 #Install piaware
