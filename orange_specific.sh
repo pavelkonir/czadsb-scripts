b6aa871e0c924e8e3e75c4c396bbdd44a8a547f5
Pavel Koníř <pavel@konir-osx.local>
2017-11-09 11:09:34 +0100

    start script after boot orange

--- orange_specific.sh
+++ orange_specific.sh
@@ -6,4 +6,7 @@ sleep 3
 
 echo "blacklist dvb_usb_rtl2832u" | sudo tee /etc/modprobe.d/rtl-sdr-blacklist.conf -a
 
+line="@reboot /home/pi/czadsb-scripts/czadsb-install.sh"
+(crontab -u root -l; echo "$line" ) | crontab -u root -
+
 echo "***** End script orange specific ******"
