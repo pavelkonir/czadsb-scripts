3d3d1f0b9e3dbfb973398b57d8f31598f8b75ef8
Pavel Koníř <pavel@konir-osx.local>
2017-11-13 14:18:59 +0100

    added rpimonitor installation script.

---
+++ addons/rpimonitor.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! -f "/etc/armbian-release" ]
+    then
+     sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 2C0D3C0F
+     sudo wget http://goo.gl/vewCLL -O /etc/apt/sources.list.d/rpimonitor.list
+     sudo apt-get update
+     sudo apt-get install rpimonitor -y
+     sudo sudo apt-get -y -f install
+fi
