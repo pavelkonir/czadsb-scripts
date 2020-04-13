df97e21b3374b7287d53efccd62b40029f4eaa87
Roman Hosek <romanhosek.cz@gmail.com>
2020-04-13 22:13:48 +0200

    user fixes

--- mm2_install.sh
+++ mm2_install.sh
@@ -6,16 +6,16 @@ sleep 3
 mm2Version="20170506"
 username="$(id -u -n)"
 
-cd /home/pi/downloads
+cd ~/downloads
 
 wget http://freedom.dicea.unifi.it/debian/pool/main/o/openssl/libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
 sudo dpkg -i libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
 
 mkdir ~/modesmixer2
-cd /home/pi/modesmixer2
+cd ~/modesmixer2
 wget https://github.com/hosek/czadsb-scripts/raw/master/modesmixer2
 sudo chmod +x modesmixer2
-sudo chown pi:pi /home/pi/modesmixer2
+sudo chown $username:$username ~/modesmixer2
 
 echo "copy czadsb starter script"
 sudo cp ~/czadsb-scripts/starters/czadsb.modesmixer.sh ~/modesmixer2/czadsb.modesmixer.sh
--- starters/autostart.sh
+++ starters/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-  sudo sed -i -e '$i \/home/pi/mlat-client/czadsb.mlat-client.sh &\n' /etc/rc.local
-  sudo sed -i -e '$i \/home/pi/n2n/czadsb.n2n.sh &\n' /etc/rc.local
-  sudo sed -i -e '$i \/home/pi/modesmixer2/czadsb.modesmixer.sh &\n' /etc/rc.local
+  sudo sed -i -e '$i ~/mlat-client/czadsb.mlat-client.sh &\n' /etc/rc.local
+  sudo sed -i -e '$i ~/n2n/czadsb.n2n.sh &\n' /etc/rc.local
+  sudo sed -i -e '$i ~/modesmixer2/czadsb.modesmixer.sh &\n' /etc/rc.local
