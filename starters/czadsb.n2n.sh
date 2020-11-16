0ff0f0773d4e8a1d49b577b9598dc50339ae6e81
pavelkonir <pavel@konir.cz>
2020-11-16 16:10:17 +0100

    maska n2n site

--- starters/czadsb.n2n.sh
+++ starters/czadsb.n2n.sh
@@ -10,7 +10,7 @@ while [ "$N2N_VPN" = "yes" ]; do
   sleep 1m
   if [ ""'ping -c 1 n2n.czadsb.cz'"" ]; then
     echo "Attempt to start VPN";
-    /usr/sbin/edge -a $N2N_IP -l n2n.czadsb.cz:82 -c adsb -k adsb123 -b &
+    /usr/sbin/edge -a $N2N_IP -s 255.255.254.0 -l n2n.czadsb.cz:82 -c adsb -k adsb123 -b &
     break;
   fi
 
