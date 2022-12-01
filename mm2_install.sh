2a707119307b1028138a782d007de9640c57bc34
Roman Hosek <roman.hosek@24i.com>
2022-12-01 20:55:02 +0100

    local libssl

--- mm2_install.sh
+++ mm2_install.sh
@@ -6,10 +6,9 @@ sleep 3
 username="$(id -u -n)"
 
 mkdir ~/modesmixer2
-cd ~/downloads
+cd ~/modesmixer2
 
-wget http://freedom.dicea.unifi.it/debian/pool/main/o/openssl/libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
-sudo dpkg -i libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
+sudo dpkg -i ~/czadsb-scripts/mm2/libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
 
 # Use device specific MM2 - 32bit on RPI or Orange H5 64bit, there are no other choices http://xdeco.org/?page_id=30
 if cat /proc/device-tree/model | grep -q 'Raspberry Pi 4'; then
@@ -25,8 +24,6 @@ else
     echo "Unable to found right version of MM2, using default one, its possible that it will not work" 2>&1
     cp ~/czadsb-scripts/mm2/modesmixer2-rpi2-3_deb9 ~/modesmixer2/modesmixer2
 fi
-
-cd ~/modesmixer2
 
 sudo chmod +x modesmixer2
 sudo chown $username:$username ~/modesmixer2
